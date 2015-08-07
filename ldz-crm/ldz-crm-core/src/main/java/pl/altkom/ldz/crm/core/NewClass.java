c5a6369fb1aac45bfa52ad45226fefd0e4e26da3
student <student@G7_1>
2015-08-07 09:36:08 +0200

    src

---
+++ ldz-crm/ldz-crm-core/src/main/java/pl/altkom/ldz/crm/core/NewClass.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2015-08-07the original author or authors.
+ */
+package pl.altkom.ldz.crm.core;
+
+/**
+ *
+ * @author Adrian Lapierre <adrian@soft-project.pl>
+ */
+public class NewClass {
+    
+}
